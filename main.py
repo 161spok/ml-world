9ca4e4aa05836709abc9c012da5f7745d7011e28
161spok <136721685+161spok@users.noreply.github.com>
2024-05-28 16:36:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,16 +22,8 @@ selezionato = sidebar_menu()
 
 match selezionato:
         case "Home":
-            st.page_link("pages/user.py", label="Your profile")
-            st.title("Metodologia da seguire")
-            st.write("E' un processo iterativo")
-            st.write("1 - Quale problema stiamo tentando di risolvere ? : 1 Problem definition")
-            st.write("2 - Quali dati abbiamo ? : 2 Data")
-            st.write("3 - Cosa definisce il successo ? : 3 Evaluation")
-            st.write("4 - Quali caratteristiche dobbiamo modellare ? : 4 - Features")
-            st.write("5 - Quale tipo di modello dovremmo utilizzare ? : 5 - Modelling")
-            st.write("6 - Cosa abbiamo sperimentato / cosa altro dobbiamo fare ? : 6 - Experiments")
-            st.write("----------------------------------------------------------------------------")
+            st.page_link("pages/home.py", label="Your profile")
+            
         case "1 Problem definition":
 
             st.header(":blue[1 Problem definition]")
