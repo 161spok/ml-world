6bc535e95c8e34973a14fc449e36eb1de6f376a6
161spok <136721685+161spok@users.noreply.github.com>
2024-05-28 16:57:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ selezionato = sidebar_menu()
 
 match selezionato:
         case "Home":
-            st.page_link("pagine/home.py", label="Home")
+            st.page_link("home.py", label="Home")
             
         case "1 Problem definition":
-            st.page_link("pagine/problemdef.py", label="Problem definition")
+            st.page_link("problemdef.py", label="Problem definition")
             
         case "2 Data":
-            st.page_link("pagine/data.py", label="Data")
+            st.page_link("data.py", label="Data")
             
