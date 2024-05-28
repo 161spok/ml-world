bbb1ecc7b9b33e2faf42ce3534943f333e19589d
161spok <136721685+161spok@users.noreply.github.com>
2024-05-28 17:47:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from streamlit_option_menu import option_menu
 st.set_page_config(layout="wide")
 dati_caricati = False
 
+def click_button():
+    #if st.button("Home", key="secondo"):
+         st.switch_page("pages/home.py")
+
+st.button('Click me', on_click=click_button)
 
 
-    
-
-
-
-if st.button("Home", key="secondo"):
-     st.switch_page("pages/home.py")
     
 
            
