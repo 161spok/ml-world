0be6a9a4b968bfd10fd6e667a6798c4a755c6750
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 15:37:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+st.subheader("Distribuzione dati")
+df = pd.read_csv('data.csv')
+df.plot()
+plt.show()
 
 st.set_page_config(layout="wide")
 dati_caricati = False
