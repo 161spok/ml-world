f2141abb0b52feb12d5ddc478b30667438805075
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 13:10:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ show_pages(
     [
         Page("main.py", "Main", "🏠"),
         Page("pages/home.py",            "Home", ":books:"),
-        Page("pages/problemdef.py",      "1 Problem definition", ":department_store:"),
+        Page("pages/problemdef.py",      "[1] Problem definition", ":department_store:"),
         #Section(name="SEZIONE  Data", icon=":globe_with_meridians:"),
         # Pages after a section will be indented
-        Page("pages/data.py", "2 Data", icon=":globe_with_meridians:"),
-        Page("pages/preelaborazione.py", "2.1 Preelaborazione", icon=":globe_with_meridians:"),
+        Page("pages/data.py", "[2] Data", icon=":globe_with_meridians:"),
+        Page("pages/preelaborazione.py", "[2.1] Preelaborazione", icon=":globe_with_meridians:"),
         # Unless you explicitly say in_section=False
-        Page("pages/duplicati.py", "2.1.1 Rimozione dei duplicati", icon=":globe_with_meridians:"),
-        Page("pages/analisi.py",         "2.2 Analisi dei dati", ":globe_with_meridians:", in_section=False),  
-        Page("pages/interpretazione.py", "2.3 Interpretazione dei dati", ":globe_with_meridians:", in_section=False),
-        Page("pages/archiviazione.py",   "2.4 Archiviazione e gestione dei dati", ":globe_with_meridians:", in_section=False),
-        Page("pages/visualizzazione.py", "2.5 Visualizzazione dei dati", ":globe_with_meridians:", in_section=False),
-        Page("pages/evaluation.py", "3 Evaluation", ":globe_with_meridians:", in_section=False),
+        Page("pages/duplicati.py", "[2.1.1] Rimozione dei duplicati", icon=":globe_with_meridians:"),
+        Page("pages/analisi.py",         "[2.2] Analisi dei dati", ":globe_with_meridians:", in_section=False),  
+        Page("pages/interpretazione.py", "[2.3] Interpretazione dei dati", ":globe_with_meridians:", in_section=False),
+        Page("pages/archiviazione.py",   "[2.4] Archiviazione e gestione dei dati", ":globe_with_meridians:", in_section=False),
+        Page("pages/visualizzazione.py", "[2.5] Visualizzazione dei dati", ":globe_with_meridians:", in_section=False),
+        Page("pages/evaluation.py", "[3] Evaluation", ":globe_with_meridians:", in_section=False),
     ]
 )    
 
