4347148f1b37c2de047409b36a31e5537fbf40a9
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 13:15:59 +0200

    Update duplicati.py

--- pages/duplicati.py
+++ pages/duplicati.py
@@ -11,10 +11,10 @@ if st.button("Submit & Process", type="primary", key="process_button") :
                                             dati_caricati = True
                                             data = pd.read_csv(uploaded_file) #path folder of the data file
                                             st.write(data)
-# Check for duplicate rows
-duplicates = data.duplicated().sum()
-st.write("Number of duplicate rows:", duplicates)
+                                            # Check for duplicate rows
+                                            duplicates = data.duplicated().sum()
+                                            st.write("Number of duplicate rows:", duplicates)
                                       
-# Removing duplicate rows
-data.drop_duplicates(inplace=True)
-st.write(data)
+                                            # Removing duplicate rows
+                                            data.drop_duplicates(inplace=True)
+                                            st.write(data)
