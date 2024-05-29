2b675f7e8c08d0d608e988369908eefb6b9db534
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 14:51:26 +0200

    Update distribuzione.py

--- pages/distribuzione.py
+++ pages/distribuzione.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
-data.boxplot()
-data.hist()
+if 'dati' not in st.session_state:
+  pass
+else:  
+  st.write(st.session_state.dati)
+  data = st.session_state.df
+  data.boxplot()
+  data.hist()
 
-tips = sns.load_dataset('tips')
-tips.plot()
+  tips = sns.load_dataset('data')
+  tips.plot()
