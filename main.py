e7a88f6e3397712e2d987b7dbfc8506c7019fb2b
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 15:43:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import pandas as pd
 #from streamlit_option_menu import option_menu
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import sys
 
 st.subheader("Distribuzione dati")
 df = pd.read_csv('data.csv')
 df.plot()
 plt.show()
-
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
 
