5538787ddd03d4066e3fa0554f89ee45fe49168c
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 10:55:08 +0200

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -9,3 +9,13 @@ st.write("4 - Quali caratteristiche dobbiamo modellare ? : 4 - Features")
 st.write("5 - Quale tipo di modello dovremmo utilizzare ? : 5 - Modelling")
 st.write("6 - Cosa abbiamo sperimentato / cosa altro dobbiamo fare ? : 6 - Experiments")
 st.write("----------------------------------------------------------------------------")
+st.write("I passi del processo sono quindi i seguenti:")
+st.write("1 - Problem definition")
+st.write("2 - Data")
+st.write("3 - Evaluation")
+st.write("4 - Features")
+st.write("5 - Modelling")
+st.write("6 - Experiments")
+
+
+
