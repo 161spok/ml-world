4ffeaf5180ba6fdcbe0682d2b0b48c97f48c9534
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 13:08:03 +0200

    Update duplicati.py

--- pages/duplicati.py
+++ pages/duplicati.py
@@ -2,6 +2,15 @@ import streamlit as st
 
 st.write("Duplicati")
 
+uploaded_file = st.file_uploader("Scegli un file", key="pdf_uploader")
+                
+if st.button("Submit & Process", type="primary", key="process_button") :
+            with st.spinner("Elaborazione ..."):
+                                        st.success("Caricamento effettuato !") 
+                                        if uploaded_file is not None:
+                                            dati_caricati = True
+                                            data = pd.read_csv(uploaded_file) #path folder of the data file
+                                            st.write(data)
 # Check for duplicate rows
 duplicates = data.duplicated().sum()
 st.write("Number of duplicate rows:", duplicates)
