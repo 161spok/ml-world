3e22e82d73132865f8ba7b7c10ca794bc572f8cb
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 12:57:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ show_pages(
         Page("pages/data.py", "2 Data", icon=":globe_with_meridians:"),
         Page("pages/preelaborazione.py", "2.1 Preelaborazione", icon=":globe_with_meridians:"),
         # Unless you explicitly say in_section=False
+        Page("pages/preelaborazione.py", "2.1.1 Rimozione dei duplicati", icon=":globe_with_meridians:"),
         
         Page("pages/analisi.py",         "2.2 Analisi dei dati", ":globe_with_meridians:", in_section=False),  
         Page("pages/interpretazione.py", "2.3 Interpretazione dei dati", ":globe_with_meridians:", in_section=False),
