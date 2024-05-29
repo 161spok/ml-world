e2d2c182be123f6ca38ac8ed7fbef8c0b0bfd7c8
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 12:52:18 +0200

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -19,8 +19,6 @@ def click_button():
     st.switch_page("pages/preelaborazione.py")
 
 #st.button("Preelaborazione dati", on_click=click_button)
-if st.button("Page 1"):
-    st.switch_page("pages/preelaborazione.py")
 # in container
 with st.container(border=True):
       col1, col2, col3, col4, col5 = st.columns(5)
