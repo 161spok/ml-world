9d858549c790922c3628b30a96189e19cb1b66de
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 14:25:10 +0200

    Update mancanti.py

--- pages/mancanti.py
+++ pages/mancanti.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
 
+st.subheader("Rimozione dei dati mancanti")
+
+if 'dati' not in st.session_state:
+  pass
+else:  
+  st.write(st.session_state.dati)
+  data = st.session_state.df
+
+  # Check for missing rows
+  missing_values = data.isna().sum()
+  st.write("Missing Values:")
+  st.write(missing_values)
+  
