8347c0516a60c656b4a94f981a8b0d471c889d99
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 13:33:30 +0200

    Update duplicati.py

--- pages/duplicati.py
+++ pages/duplicati.py
@@ -3,12 +3,15 @@ import pandas as pd
 
 st.subheader("Rimozione dei duplicati")
 
-data = st.write(st.session_state.dati)
+if 'dati' not in st.session_state:
+  pass
+else:  
+  data = st.write(st.session_state.dati)
 
-# Check for duplicate rows
-duplicates = data.duplicated().sum()
-st.write("Number of duplicate rows:", duplicates)
+  # Check for duplicate rows
+  duplicates = data.duplicated().sum()
+  st.write("Number of duplicate rows:", duplicates)
                                       
-# Removing duplicate rows
-data.drop_duplicates(inplace=True)
+  # Removing duplicate rows
+  data.drop_duplicates(inplace=True)
                                             
