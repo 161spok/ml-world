3eba9afee7f6fabb4f8c27724b554079271d56e8
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 12:55:47 +0200

    Update preelaborazione.py

--- pages/preelaborazione.py
+++ pages/preelaborazione.py
@@ -1,15 +1,15 @@
-                import stramlit as st
+import stramlit as st
 
+st.subheader("Preelaborazione dati")
+predati = st.text_area('DESCRIZIONE', 'Questa fase prevede la pulizia, il filtraggio e la trasformazione dei dati per renderli idonei per ulteriori analisi. '
+'Ciò può includere la rimozione dei valori mancanti, il ridimensionamento o la normalizzazione dei dati o la loro conversione in un formato diverso.')
+st.text_area('Preparazione', 'I dati raccolti possono essere in una forma grezza che non può essere alimentata direttamente alla macchina. ' 
+'Quindi, questo è un processo di raccolta di set di dati da diverse fonti, l\'analisi di questi set di dati e ' 
+'quindi la costruzione di un nuovo set di dati per ulteriore elaborazione ed esplorazione. Questa preparazione ' 
+'può essere eseguita manualmente o dall\' approccio automatico. I dati possono anche essere preparati in forme ' 
+'numeriche che velocizzerebbero l\' apprendimento del modello. ' 
+'Esempio: Una immagine può essere convertita in una matrice di dimensioni NXN, il valore di ciascuna cella indicherà il pixel dell\' immagine.')
 
-                st.subheader("Preelaborazione dati")
-                predati = st.text_area('DESCRIZIONE', 'Questa fase prevede la pulizia, il filtraggio e la trasformazione dei dati per renderli idonei per ulteriori analisi. '
-                'Ciò può includere la rimozione dei valori mancanti, il ridimensionamento o la normalizzazione dei dati o la loro conversione in un formato diverso.')
-                st.text_area('Preparazione', 'I dati raccolti possono essere in una forma grezza che non può essere alimentata direttamente alla macchina. ' 
-                'Quindi, questo è un processo di raccolta di set di dati da diverse fonti, l\'analisi di questi set di dati e ' 
-                'quindi la costruzione di un nuovo set di dati per ulteriore elaborazione ed esplorazione. Questa preparazione ' 
-                'può essere eseguita manualmente o dall\' approccio automatico. I dati possono anche essere preparati in forme ' 
-                'numeriche che velocizzerebbero l\' apprendimento del modello. ' 
-                'Esempio: Una immagine può essere convertita in una matrice di dimensioni NXN, il valore di ciascuna cella indicherà il pixel dell\' immagine.')
 #---------------#                
                 with st.container(border=True):
                               col1, col2, col3, col4, col5, col6, col7, col8, col9  = st.columns([1,1,1,1,1,1,1,1,1])
