b4bfed1f5ea0355ae72b3779a6c2e5dd99994308
161spok <136721685+161spok@users.noreply.github.com>
2024-05-29 13:00:05 +0200

    Update duplicati.py

--- pages/duplicati.py
+++ pages/duplicati.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.write("Duplicati")
+if duplicati:
+                    # Check for duplicate rows
+                    duplicates = data.duplicated().sum()
+                    st.write("Number of duplicate rows:", duplicates)
+                                      
+                    # Removing duplicate rows
+                    data.drop_duplicates(inplace=True)
+                    st.write(data)
