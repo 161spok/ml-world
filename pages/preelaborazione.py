21884099c568e8efaeb83ae2a58aecf3d34f52c7
161spok <136721685+161spok@users.noreply.github.com>
2024-05-30 18:01:30 +0200

    Update preelaborazione.py

--- pages/preelaborazione.py
+++ pages/preelaborazione.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.subheader("Data - Preelaborazione dati")
+st.subheader("Preelaborazione dati")
 
 predati = st.text_area('DESCRIZIONE', 'Questa fase prevede la pulizia, il filtraggio e la trasformazione dei dati per renderli idonei per ulteriori analisi. '
 'Ciò può includere la rimozione dei valori mancanti, il ridimensionamento o la normalizzazione dei dati o la loro conversione in un formato diverso.')
