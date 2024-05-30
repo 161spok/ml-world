05bdcfc256ad8ed6e7f9c95e81b71614608cefa2
161spok <136721685+161spok@users.noreply.github.com>
2024-05-30 17:59:07 +0200

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -37,7 +37,8 @@ if st.button("Submit & Process", type="primary", key="process_button") :
                                             dati_caricati = True
                                             data = load_data(uploaded_file)
                                             #data = pd.read_csv(uploaded_file) #path folder of the data file
-                                            st.write(data)
+                                            #st.write(data)
+                                            st.write(data.shape())
                                             #if 'dati' not in st.session_state:
                                             #        st.session_state['dati'] = 'caricati'
                                             #        st.session_state.df = data
