2532afa104c03f1f4f2831d5bba1ee07793b4db6
161spok <136721685+161spok@users.noreply.github.com>
2024-05-30 10:01:58 +0200

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -10,6 +10,7 @@ uploaded_file = st.file_uploader("Scegli un file", key="pdf_uploader")
 
 @st.cache
 def load_data(uploaded_file):
+    df=""
     df = pd.read_csv(uploaded_file) #path folder of the data file
     if 'dati' not in st.session_state:
                                                     st.session_state['dati'] = 'caricati'
