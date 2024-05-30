7070fb226a6cb42f6fe2490ffdeaab7fcd8af1f8
161spok <136721685+161spok@users.noreply.github.com>
2024-05-30 18:50:38 +0200

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -9,17 +9,51 @@ dati = st.text_area('DESCRIZIONE', 'Questo è il processo di raccolta di dati da
 uploaded_file = st.file_uploader("Scegli un file", key="pdf_uploader")
 
 
+#----------------------------------------------------------------------------------
+import streamlit.components.v1 as components
+
+modal = Modal(
+    "Demo Modal", 
+    key="demo-modal",
+    
+    # Optional
+    padding=20,    # default value
+    max_width=744  # default value
+)
+
+open_modal = st.button("Open")
+if open_modal:
+    modal.open()
+
+if modal.is_open():
+    with modal.container():
+        st.write("Text goes here")
+
+        html_string = '''
+        <h1>HTML string in RED</h1>
+
+        <script language="javascript">
+          document.querySelector("h1").style.color = "red";
+        </script>
+        '''
+        components.html(html_string)
+
+        st.write("Some fancy text")
+        value = st.checkbox("Check me")
+        st.write(f"Checkbox checked: {value}")
+#---------------------------------------------------------------------------------
+
 @st.experimental_dialog("Cache")
 def mess(item):
     st.write(f"Cache empty !")
     
     if st.button("Ok"):        
         st.rerun()
-
+#----------------------------------------------------------------------------------
 @st.cache
 def load_data(uploaded_file):
     st.cache_data.clear()
-    mess("A")
+    #mess("A")
   
     st.cache_resource.clear()
     df=""
