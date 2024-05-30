3c8218e752f29ef9a26156617f5301510e326100
161spok <136721685+161spok@users.noreply.github.com>
2024-05-30 17:49:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ show_pages(
         Page("pages/data.py", "[2] Data", icon=":globe_with_meridians:"),
         Page("pages/preelaborazione.py", "[2.1] Preelaborazione", icon=":globe_with_meridians:"),
         # Unless you explicitly say in_section=False
-        Page("pages/distribuzione.py", "[2.1.0] Distribuzione dei dati", icon=":globe_with_meridians:"),
-        Page("pages/duplicati.py", "[2.1.1] Rimozione dei duplicati", icon=":globe_with_meridians:"),
-        Page("pages/indesiderate.py", "[2.1.2] Rimozione dei dati indesiderati", icon=":globe_with_meridians:"),
-        Page("pages/mancanti.py", "[2.1.3] Rimozione dei dati mancanti", icon=":globe_with_meridians:"),
+        Page("pages/distribuzione.py", "[2.1.0] Distribuzione dei dati", icon=":chart_with_upwards_trend:"),
+        Page("pages/duplicati.py", "[2.1.1] Rimozione dei duplicati", icon=":chart_with_downwards_trend:"),
+        Page("pages/indesiderate.py", "[2.1.2] Rimozione dei dati indesiderati", icon=":memo:"),
+        Page("pages/mancanti.py", "[2.1.3] Rimozione dei dati mancanti", icon=":signal_strength:"),
         
-        Page("pages/analisi.py",         "[2.2] Analisi dei dati", ":globe_with_meridians:", in_section=False),  
-        Page("pages/interpretazione.py", "[2.3] Interpretazione dei dati", ":globe_with_meridians:", in_section=False),
-        Page("pages/archiviazione.py",   "[2.4] Archiviazione e gestione dei dati", ":globe_with_meridians:", in_section=False),
-        Page("pages/visualizzazione.py", "[2.5] Visualizzazione dei dati", ":globe_with_meridians:", in_section=False),
-        Page("pages/evaluation.py", "[3] Evaluation", ":globe_with_meridians:", in_section=False),
+        Page("pages/analisi.py",         "[2.2] Analisi dei dati", ":arrows_counterclockwise:", in_section=False),  
+        Page("pages/interpretazione.py", "[2.3] Interpretazione dei dati", ":desktop_computer:", in_section=False),
+        Page("pages/archiviazione.py",   "[2.4] Archiviazione e gestione dei dati", ":oil_drum:", in_section=False),
+        Page("pages/visualizzazione.py", "[2.5] Visualizzazione dei dati", ":red_envelope:", in_section=False),
+        Page("pages/evaluation.py", "[3] Evaluation", ":hourglass_flowing_sand:", in_section=False),
     ]
 )    
          
