23687945ba8eeafec5206a8bf8436b34610eb6a9
161spok <136721685+161spok@users.noreply.github.com>
2024-05-30 18:54:30 +0200

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -8,40 +8,6 @@ st.subheader("Raccolta dati")
 dati = st.text_area('DESCRIZIONE', 'Questo è il processo di raccolta di dati da varie fonti, come sensori, database o altri sistemi. I dati possono essere strutturati o non strutturati e possono presentarsi in vari formati come testo, immagini o audio.')
 uploaded_file = st.file_uploader("Scegli un file", key="pdf_uploader")
 
-
-#----------------------------------------------------------------------------------
-import streamlit.components.v1 as components
-from streamlit_modal import Modal
-
-modal = Modal(
-    "Demo Modal", 
-    key="demo-modal",
-    
-    # Optional
-    padding=20,    # default value
-    max_width=744  # default value
-)
-
-open_modal = st.button("Open")
-if open_modal:
-    modal.open()
-
-if modal.is_open():
-    with modal.container():
-        st.write("Text goes here")
-
-        html_string = '''
-        <h1>HTML string in RED</h1>
-
-        <script language="javascript">
-          document.querySelector("h1").style.color = "red";
-        </script>
-        '''
-        components.html(html_string)
-
-        st.write("Some fancy text")
-        value = st.checkbox("Check me")
-        st.write(f"Checkbox checked: {value}")
 #---------------------------------------------------------------------------------
 
 @st.experimental_dialog("Cache")
