e167a316718e20c21970579180ab7647222ae800
161spok <136721685+161spok@users.noreply.github.com>
2024-05-31 14:14:33 +0200

    Update datacleaning.py

--- pages/datacleaning.py
+++ pages/datacleaning.py
@@ -5,3 +5,9 @@ import pandas as pd
 import io
 
 st.page_link("https://www.geeksforgeeks.org/data-cleansing-introduction/?ref=lbp", label="Data cleansing", icon="🌎")
+
+st.write("Rimozione di osservazioni indesiderate")
+st.write("Correzione degli errori di struttura")
+st.write("Correzione dei duplicati")
+st.write("Gestione dei valori anomali indesiderati")
+st.write("Gestione dei dati mancanti")
