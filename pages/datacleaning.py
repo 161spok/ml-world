56785a8b9feaca1638463ea15a82e4ed6c7ba6a8
161spok <136721685+161spok@users.noreply.github.com>
2024-05-31 14:10:43 +0200

    Update datacleaning.py

--- pages/datacleaning.py
+++ pages/datacleaning.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import io
 
+st.page_link("https://www.geeksforgeeks.org/data-cleansing-introduction/?ref=lbp", label="Data cleansing", icon="🌎")
