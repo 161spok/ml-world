65dd61c30ec2f9594a27c5dab597c33c1c01ae34
161spok <136721685+161spok@users.noreply.github.com>
2024-05-31 11:05:59 +0200

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
+
 st.header(":blue[2 Data]")
              
 st.subheader("Raccolta dati")
