3ca19e04da7b25c37b7aa5ead8f743fb4a9cb580
161spok <136721685+161spok@users.noreply.github.com>
2024-06-07 12:35:04 +0200

    Update preelaborazione.py

--- pages/preelaborazione.py
+++ pages/preelaborazione.py
@@ -16,26 +16,112 @@ st.text_area('Preparazione', 'I dati raccolti possono essere in una forma grezza
 
 #st.write("La preelaborazione dei dati prevede i seguenti passi:")
 st.markdown("**La preelaborazione dei dati prevede i seguenti passi:**")
-st.write("Distribuzione dati")  
 
-st.write("Rimozione dei duplicati")                                 
-                             
-st.write("Rimozione di osservazioni indesiderate")
-                             
-st.write("Correzione degli errori di struttura")
-                             
-st.write("Gestione dei valori anomali")
-                             
-st.write("Gestione dei dati mancanti")
-                             
-st.write("Trasformazione dei dati")
-
-st.write("Pulizia dei dati di testo")
-                              
-st.write("Standardizzazione dei dati - Feature Scaling")
-                             
-st.write("Formattazione dei dati")
-                              
-st.write("Codifica delle etichette")
+# -------------------------------------------- https://www.geeksforgeeks.org/ml-handling-imbalanced-data-with-smote-and-near-miss-algorithm-in-python/
+#st.write("Distribuzione dati")  
+with st.expander("Distribuzione dati"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+  
+#st.write("Rimozione dei duplicati")                                 
+with st.expander("Rimozione dei duplicati"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+      
+#st.write("Rimozione di osservazioni indesiderate")
+with st.expander("Rimozione di osservazioni indesiderate"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')          
+  
+#st.write("Correzione degli errori di struttura")
+with st.expander("Correzione degli errori di struttura"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')                       
+  
+#st.write("Gestione dei valori anomali")
+with st.expander("Gestione dei valori anomali"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')  
+  
+#st.write("Gestione dei dati mancanti")
+with st.expander("Gestione dei dati mancanti"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')                               
+#st.write("Trasformazione dei dati")
+with st.expander("Trasformazione dei dati"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')  
+#st.write("Pulizia dei dati di testo")
+with st.expander("Pulizia dei dati di testo"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')                                
+#st.write("Standardizzazione dei dati - Feature Scaling")
+with st.expander("Standardizzazione dei dati - Feature Scaling"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')                               
+#st.write("Formattazione dei dati")
+with st.expander("Formattazione dei dati"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')                                
+#st.write("Codifica delle etichette")
+with st.expander("Codifica delle etichette"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')  
+#st.write("One-hot encoding")
+with st.expander("One-hot encoding"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')  
+#st.write("Gestione dei dati sbilanciati con SMOTE e l'algoritmo Near Miss")
+with st.expander("Gestione dei dati sbilanciati con SMOTE e l'algoritmo Near Miss"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')  
+#st.write("Trappola delle variabili fittizie nei modelli di regressione")
+with st.expander("Trappola delle variabili fittizie nei modelli di regressione"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')  
+         
 #---------------# 
 
