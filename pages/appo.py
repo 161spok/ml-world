de779e94196b6d7dacee8dc40e2cfe761a841cfd
161spok <136721685+161spok@users.noreply.github.com>
2024-06-10 16:28:11 +0200

    Update appo.py

--- pages/appo.py
+++ pages/appo.py
@@ -9,14 +9,14 @@ if 'dati' not in st.session_state:
 	if st.button("Submit & Process", type="primary", key="process_button") :
 		with st.spinner("Elaborazione ..."):
 			st.success("Caricamento effettuato !") 
-			st.info("Caricamento effettuato !")
+			#st.info("Caricamento effettuato !")
 			if uploaded_file is not None:                                           
 				dati_caricati = True                                            
 				df = pd.read_csv(uploaded_file) #path folder of the data file
 				st.write(df)											
 				 					
 				for col in df.columns:
-					st.write(col)						
+					#st.write(col)						
 					campi = list(df.columns.values)
 					st.write(campi)
 
