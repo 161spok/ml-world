8a4817c12b4d658f6a3bc92c3352ea503bdc6474
161spok <136721685+161spok@users.noreply.github.com>
2024-06-10 11:53:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,18 +38,26 @@ show_pages(
         Page("pages/data.py", "[2] Data", icon=":globe_with_meridians:"),
         Page("pages/preelaborazione.py", "[2.1] Preelaborazione", icon=":globe_with_meridians:"),
         # Unless you explicitly say in_section=False
-        Page("pages/distribuzione.py", "[2.1.0] Data cleaning ", icon=":chart_with_upwards_trend:"),
-        Page("pages/datacleaning.py", "[2.1.0.1] Distribuzione dei dati", icon=":chart_with_upwards_trend:"),
+        Page("pages/distribuzione.py",   "[2.1.0] Data cleaning ", icon=":chart_with_upwards_trend:"),
+        Page("pages/datacleaning.py",    "[2.1.0.1] Distribuzione dei dati", icon=":chart_with_upwards_trend:"),
         
-        Page("pages/duplicati.py", "[2.1.1] Rimozione dei duplicati", icon=":chart_with_downwards_trend:"),
-        Page("pages/indesiderate.py", "[2.1.2] Rimozione dei dati indesiderati", icon=":memo:"),
-        Page("pages/mancanti.py", "[2.1.3] Rimozione dei dati mancanti", icon=":signal_strength:"),
+        Page("pages/duplicati.py",       "[2.1.0.2] Rimozione dei duplicati", icon=":chart_with_downwards_trend:"),
+        Page("pages/indesiderate.py",    "[2.1.0.3] Correzione degli errori di struttura", icon=":memo:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] Gestione dei valori anomali", icon=":signal_strength:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] Gestione dei mancanti", icon=":signal_strength:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] Trasformazione dei dati", icon=":signal_strength:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] Pulizia dei dati di testo", icon=":signal_strength:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] Standardizzazione dei dati - feature scaling", icon=":signal_strength:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] Codifica delle etichette", icon=":signal_strength:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] One-hot encoding", icon=":signal_strength:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] Gestione dei dati sbilanciati", icon=":signal_strength:"),
+        Page("pages/mancanti.py",        "[2.1.0.4] Trappola delle variabili fittizie nei modelli di regressione", icon=":signal_strength:"),
         
         Page("pages/analisi.py",         "[2.2] Analisi dei dati", ":arrows_counterclockwise:", in_section=False),  
         Page("pages/interpretazione.py", "[2.3] Interpretazione dei dati", ":desktop_computer:", in_section=False),
         Page("pages/archiviazione.py",   "[2.4] Archiviazione e gestione dei dati", ":oil_drum:", in_section=False),
         Page("pages/visualizzazione.py", "[2.5] Visualizzazione dei dati", ":red_envelope:", in_section=False),
-        Page("pages/evaluation.py", "[3] Evaluation", ":hourglass_flowing_sand:", in_section=False),
+        Page("pages/evaluation.py",      "[3] Evaluation", ":hourglass_flowing_sand:", in_section=False),
     ]
 )    
 file = st.sidebar.file_uploader('Upload',type=["pdf","txt"])         
