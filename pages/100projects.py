6581823a8ec37f979d79a5dfad2f70d2187edd7c
161spok <136721685+161spok@users.noreply.github.com>
2024-06-11 21:28:14 +0200

    Update 100projects.py

--- pages/100projects.py
+++ pages/100projects.py
@@ -1,1 +1,5 @@
+import streamlit as st
+
+# https://www.geeksforgeeks.org/machine-learning-projects/?ref=header_search
+# https://www.geeksforgeeks.org/top-data-science-projects/?ref=header_search
 
