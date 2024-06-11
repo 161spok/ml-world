7aa61be2eaa97719e4a439fb8ab8828a0aee999e
161spok <136721685+161spok@users.noreply.github.com>
2024-06-11 21:20:38 +0200

    Update categoriche.py

--- pages/categoriche.py
+++ pages/categoriche.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.code(f"""
+# Categorical columns 
+cat_col = [col for col in df.columns if df[col].dtype == 'object'] 
+print('Categorical columns :',cat_col) 
+# Numerical columns 
+num_col = [col for col in df.columns if df[col].dtype != 'object'] 
+print('Numerical columns :',num_col)
+""")
