172aea9566817b3e7ec5c92b2efd9410f597654e
161spok <136721685+161spok@users.noreply.github.com>
2024-06-12 13:40:13 +0200

    Update 100projects.py

--- pages/100projects.py
+++ pages/100projects.py
@@ -6,3 +6,4 @@ import streamlit as st
 st.page_link("https://www.geeksforgeeks.org/machine-learning-projects/?ref=header_search", label="Vai a 100 progetti Machine Learning", icon="🌎")
 st.page_link("https://www.geeksforgeeks.org/top-data-science-projects/?ref=header_search", label="Vai a 100 progetti Data Science", icon="🌎")
 
+st.page_link("https://machinelearningmastery.com/machine-learning-in-python-step-by-step/", label="Vai a Progetto", icon="🌎")
