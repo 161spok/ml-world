e3a1a07a72b6d9ff77e11d8f8c304413859dd985
161spok <136721685+161spok@users.noreply.github.com>
2024-06-12 15:56:13 +0200

    Update preelaborazione.py

--- pages/preelaborazione.py
+++ pages/preelaborazione.py
@@ -564,7 +564,8 @@ Le variabili qualitative possono essere rappresentate anche da stringhe, in ques
 In inglese le variabili qualitative sono conosciute come categorical variables (ordinal e nominal).
     ''') 
     st.image('tipologia_variabili.PNG', caption='')
-     st.page_link("https://github.com/ProfAI/ml00/blob/master/2%20-%20Datasets%20e%20data%20preprocessing/Operare%20su%20variabili%20categoriche.ipynb", label="Variabili categoriche", icon="🏠")
+    st.page_link("https://github.com/ProfAI/ml00/blob/master/2%20-%20Datasets%20e%20data%20preprocessing/Operare%20su%20variabili%20categoriche.ipynb", label="Variabili categoriche", icon="🏠")
+    
 with st.expander("**Trasformazione dei dati**"):
     st.write('''
         The chart above shows some numbers I picked for you.
