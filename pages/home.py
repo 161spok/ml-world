9f4b3abd59742d371c6a924befdaa1fcfef8fc71
161spok <136721685+161spok@users.noreply.github.com>
2024-06-12 11:50:28 +0200

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Metodologia da seguire")
 
 st.markdown(''':blue-background[E' un processo iterativo] ''')
-
+st.image('imag.png', caption='Processo')
 st.write("1 - Quale problema stiamo tentando di risolvere ?")
 st.write("2 - Quali dati abbiamo ?")
 st.write("3 - Cosa definisce il successo ?")
