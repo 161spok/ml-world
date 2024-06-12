adaa534aa36edbf919f4879364a96995e8c7a572
161spok <136721685+161spok@users.noreply.github.com>
2024-06-12 17:04:49 +0200

    Update evaluation.py

--- pages/evaluation.py
+++ pages/evaluation.py
@@ -6,6 +6,8 @@ st.write('''L\'apprendimento supervisionato è l\'attività di apprendimento aut
 I dati forniti sono etichettati. Sia i problemi di classificazione che quelli di regressione sono problemi di apprendimento supervisionato.\n
 Esempio: considera i seguenti dati relativi ai pazienti che entrano in una clinica. I dati consistono nel sesso e nell\' età dei pazienti e ogni paziente è etichettato come \'sano\' o \'malato\'.
 \n
+''')
+st.code(f"""
 Genere        Età        Etichetta\n
 M             48         malato\n
 M             67         malato\n
@@ -14,7 +16,7 @@ M             49         malato\n
 F             32         in salute\n
 M             34         in salute\n
 M             21         in salute\n
-''')
+""")
 
 st.write('''il modello o l'algoritmo viene presentato con input di esempio e i relativi output desiderati, quindi trova modelli e connessioni tra l'input e l'output. L’obiettivo è apprendere una regola generale che associa gli input agli output. Il processo di addestramento continua finché il modello non raggiunge il livello di precisione desiderato sui dati di addestramento. Alcuni esempi di vita reale sono:
 -Classificazione delle immagini: ti alleni con immagini/etichette. Quindi in futuro dai una nuova immagine aspettandoti che il computer riconosca il nuovo oggetto.
