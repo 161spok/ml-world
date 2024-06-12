e6e9917caa958f4148427c641507a61b6877d38f
161spok <136721685+161spok@users.noreply.github.com>
2024-06-12 17:02:59 +0200

    Update evaluation.py

--- pages/evaluation.py
+++ pages/evaluation.py
@@ -5,15 +5,15 @@ st.write('''Abbiamo tre tipologie: supervised learning, unsupervised learning, e
 st.write('''L\'apprendimento supervisionato è l\'attività di apprendimento automatico di apprendere una funzione che associa un input a un output sulla base di coppie input-output di esempio. 
 I dati forniti sono etichettati. Sia i problemi di classificazione che quelli di regressione sono problemi di apprendimento supervisionato.\n
 Esempio: considera i seguenti dati relativi ai pazienti che entrano in una clinica. I dati consistono nel sesso e nell\' età dei pazienti e ogni paziente è etichettato come \'sano\' o \'malato\'.
-
-Genere        Età        Etichetta
-M             48         malato
-M             67         malato
-F             53         in salute
-M             49         malato
-F             32         in salute
-M             34         in salute
-M             21         in salute
+\n
+Genere        Età        Etichetta\n
+M             48         malato\n
+M             67         malato\n
+F             53         in salute\n
+M             49         malato\n
+F             32         in salute\n
+M             34         in salute\n
+M             21         in salute\n
 ''')
 
 st.write('''il modello o l'algoritmo viene presentato con input di esempio e i relativi output desiderati, quindi trova modelli e connessioni tra l'input e l'output. L’obiettivo è apprendere una regola generale che associa gli input agli output. Il processo di addestramento continua finché il modello non raggiunge il livello di precisione desiderato sui dati di addestramento. Alcuni esempi di vita reale sono:
