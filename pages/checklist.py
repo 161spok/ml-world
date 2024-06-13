81f3031cbbedb99ab91b317e19783984c7c87159
161spok <136721685+161spok@users.noreply.github.com>
2024-06-13 20:49:25 +0200

    Update checklist.py

--- pages/checklist.py
+++ pages/checklist.py
@@ -33,7 +33,9 @@ st.markdown(pdf_display, unsafe_allow_html=True)
 
 import base64
 from pathlib import Path
-pdf_path = "ChecklistProgettoMachineLearningPython.pdf"
+
+pdf_path = Path("ChecklistProgettoMachineLearningPython.pdf")
+
 base64_pdf = base64.b64encode(pdf_path.read_bytes()).decode("utf-8")
 pdf_display = f"""
     <iframe src="data:application/pdf;base64,{base64_pdf}" width="800px" height="2100px" type="application/pdf"></iframe>
