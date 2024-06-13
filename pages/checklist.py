e0cabecc0392d46fc483f577cf58ebc306ef6225
161spok <136721685+161spok@users.noreply.github.com>
2024-06-13 20:23:03 +0200

    Update checklist.py

--- pages/checklist.py
+++ pages/checklist.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import os
-import base64
+
 # from streamlit_pdf_viewer import pdf_viewer
 # è possibile utilizzare questo componente per gestire i file pdf
 # https://github.com/lfoppiano/structure-vision/blob/main/streamlit_app.py
@@ -9,9 +8,6 @@ import base64
 st.header(":blue[4 Machine learning]")
 st.subheader("4.1 Checklist")
 
-file_path = "ChecklistProgettoMachineLearningPython.pdf"
-#os.chdir("/mount/src/ml-world/pages")
-#st.write(os.getcwd())
 
 """
 import PyPDF2
@@ -27,11 +23,9 @@ if file_path is not None:
     # Display the content
     st.write(content)
 """    
-with open(file_path,"rb") as f:
-#with open(file_path,"rb") as f:
-    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    
-pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+pdf_file = "ChecklistProgettoMachineLearningPython.pdf"
+base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
 st.markdown(pdf_display, unsafe_allow_html=True)
 
 #with st.expander("**Checklist**"): 
