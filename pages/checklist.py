cf8c2c2db0bf7bed8df7313b8300ef711a8efa72
161spok <136721685+161spok@users.noreply.github.com>
2024-06-13 21:26:20 +0200

    Update checklist.py

--- pages/checklist.py
+++ pages/checklist.py
@@ -38,6 +38,7 @@ def ViewPDF(wch_fl):
     with open(wch_fl,"rb") as pdf_file:
         base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
         pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1000" height="500" type="application/pdf">' 
+        st.write(pdf_display)
         st.markdown(pdf_display, unsafe_allow_html=True)
 
 #ViewPDF("./ChecklistProgettoMachineLearningPython.pdf") 
