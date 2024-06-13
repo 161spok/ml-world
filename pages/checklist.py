7bc29be9cc01952be85ed8a8b33497406f3035a9
161spok <136721685+161spok@users.noreply.github.com>
2024-06-13 19:57:42 +0200

    Update checklist.py

--- pages/checklist.py
+++ pages/checklist.py
@@ -8,17 +8,27 @@ import streamlit.components.v1 as components
 st.header(":blue[4 Machine learning]")
 st.subheader("4.1 Checklist")
 
+file_path = "ChecklistProgettoMachineLearningPython.pdf"
+"""
 import PyPDF2
-file = "ChecklistProgettoMachineLearningPython.pdf"
-if file is not None:
+
+file_path = "ChecklistProgettoMachineLearningPython.pdf"
+if file_path is not None:
     # Read the PDF file
-    pdf_reader = PdfReader(file)
+    pdf_reader = PdfReader(file_path)
     # Extract the content
     content = ""
     for page in range(pdf_reader.getNumPages()):
         content += pdf_reader.getPage(page).extractText()
     # Display the content
     st.write(content)
+"""    
+with open(file_path,"rb") as f:
+#with open(file_path,"rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    
+pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+st.markdown(pdf_display, unsafe_allow_html=True)
 
 #with st.expander("**Checklist**"): 
      #st.page_link("\ChecklistProgettoMachineLearningPython.pdf", label="pdf", icon="🏠")
