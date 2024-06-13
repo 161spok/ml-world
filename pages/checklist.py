0cee485b5b255b1120c809b34700cd479a2617d6
161spok <136721685+161spok@users.noreply.github.com>
2024-06-13 19:25:04 +0200

    Update checklist.py

--- pages/checklist.py
+++ pages/checklist.py
@@ -3,3 +3,7 @@ import streamlit.components.v1 as components
 
 st.header(":blue[4 Machine learning]")
 st.subheader("4.1 Checklist")
+
+with st.expander("**Checklist**"): 
+     st.page_link("Checklist Progetto Machine Learning e Python.pdf", label="pdf", icon="🏠")
+     #components.iframe("https://www.diariodiunanalista.it/posts/analisi-esplorativa-dei-dati-con-python-e-pandas/", height = 500, scrolling = True)
