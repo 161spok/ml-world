4565520019fe53ceff639419c9cca87f5e3abf87
161spok <136721685+161spok@users.noreply.github.com>
2024-06-17 19:40:50 +0200

    Update distribuzione.py

--- pages/distribuzione.py
+++ pages/distribuzione.py
@@ -24,12 +24,12 @@ else:
     st.page_link("https://community.sisense.com/t5/knowledge/test-for-normal-distribution-of-data-with-python/ta-p/9434", label="Test con Pandas", icon="🏠")
     
   #df = st.session_state.df
-  if 'df' not in st.session_state:
+if 'df' not in st.session_state:
       df = st.session_state['df']
       st.write("Test 1")
       st.write(df)
       st.write("-------------------------------------------------------------------------")
-  else:
+else:
       df = st.session_state['df']
       st.write("Test 2")
       st.write(df)
@@ -46,120 +46,107 @@ else:
       st.write(mean2)
   
   # -----------------------------------------distribuzione dei dati
-  import numpy as np
-  import matplotlib.pyplot as plt
-  from scipy.stats import norm
-  
-  # Carica i dati dal file CSV
-  #st.dataframe(df)  # Same as st.write(df)
-  #data = st.dataframe(df)
-  
-  # Calcola la media e la deviazione standard dei dati
-  #mean = np.mean(data)
-  #mean = data.mean(axis = 1, skipna = True)
-  uploaded_file = st.file_uploader("Scegli un file", key="pdf_uploader")
-  if st.button("Submit & Process", type="primary", key="process_button") :
-            with st.spinner("Elaborazione ..."):
-                                        st.success("Caricamento effettuato !") 
-                                        if uploaded_file is not None:                                           
-                                           
-                                            data = pd.read_csv(uploaded_file) #path folder of the data file
-                                            st.write(data)
-                                            tdata = pd.DataFrame(data)
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+                                            
+data = pd.read_csv(uploaded_file) #path folder of the data file
+st.write(data)
+tdata = pd.DataFrame(data)
                                           
                                           # media tutte le colonne escluso quelle non numeriche
-                                            media = tdata.mean(axis = 0, skipna = False, numeric_only=True)
+media = tdata.mean(axis = 0, skipna = False, numeric_only=True)
                                           
                                           # media di colonne selezionate
                                             #mean1 = tdata.iloc[:,0].mean() #calcola la media della prima colonna
-                                            mean2 = tdata.iloc[:,3].mean() # calcola la media della seconda colonna
-                                            st.write("Media della colonna 4")
-                                            st.write(mean2)
+mean2 = tdata.iloc[:,3].mean() # calcola la media della seconda colonna
+st.write("Media della colonna 4")
+st.write(mean2)
                                             
                                           # deviazione standard 
               # In generale, una deviazione standard alta indica una maggiore variabilità dei dati, mentre una deviazione standard bassa indica una minore variabilità.
-                                            std_dev = tdata.std(numeric_only=True)
-                                            st.write("Deviazione standard su tutte le colonne")
-                                            st.write(std_dev)
+std_dev = tdata.std(numeric_only=True)
+st.write("Deviazione standard su tutte le colonne")
+st.write(std_dev)
                                           
                                           # deviazione standard su singola colonna
-                                            std_dev_col = tdata.iloc[:,3].std()
+std_dev_col = tdata.iloc[:,3].std()
                                             #  df['age'].std()
-                                            st.write("Deviazione standard su colonna 4")
-                                            st.write(std_dev_col)
+st.write("Deviazione standard su colonna 4")
+st.write(std_dev_col)
 
                                             # Generate values for the x-axis
                                             # Calcola la densità di probabilità (PDF) della distribuzione normale
-                                            x = np.linspace(media - 3*std_dev, media + 3*std_dev, 1000)
-                                            pdf = norm.pdf(x, media, std_dev)
+x = np.linspace(media - 3*std_dev, media + 3*std_dev, 1000)
+pdf = norm.pdf(x, media, std_dev)
                                             
                                             # Visualizza la PDF
-                                            plt.plot(x, pdf)
-                                            plt.title('Distribuzione Normale dei Dati')
-                                            plt.xlabel('Valore')
-                                            plt.ylabel('Probabilità')
-                                            st.pyplot(plt.gcf())
+plt.plot(x, pdf)
+plt.title('Distribuzione Normale dei Dati')
+plt.xlabel('Valore')
+plt.ylabel('Probabilità')
+st.pyplot(plt.gcf())
                                             
                                             #------------------------------------------------------------------------                                        
-                                            x = np.linspace(0, 10000, 500)
-                                            arr = data['Type']
-                                            fig, ax = plt.subplots()
-                                            plt.title('Distribuzione Normale dei Dati colonna Type')
-                                            ax.hist(arr, bins=20)                                        
-                                            st.pyplot(fig)
+x = np.linspace(0, 10000, 500)
+arr = data['Type']
+fig, ax = plt.subplots()
+plt.title('Distribuzione Normale dei Dati colonna Type')
+ax.hist(arr, bins=20)                                        
+st.pyplot(fig)
                                             #------------------------------------------------------------------------ 
-                                            figsize(7, 5)
-                                            plt.hist(data['Type'], color='blue', edgecolor='black', bins=int(45/1))
-                                            plt.xlabel('Quantità')
-                                            plt.ylabel('Tipologia')
-                                            plt.title('Housing prices frequencies')
+figsize(7, 5)
+plt.hist(data['Type'], color='blue', edgecolor='black', bins=int(45/1))
+plt.xlabel('Quantità')
+plt.ylabel('Tipologia')
+plt.title('Housing prices frequencies')
                                             st.pyplot(plt.gcf())
 
   
-  col1, col2 = st.columns(2)
-  with col1:
+col1, col2 = st.columns(2)
+with col1:
     st.header("Head")
     st.write(df.head())
   
-  with col2:
+with col2:
     st.header("Info")
     buffer = io.StringIO()
     df.info(buf=buffer)
     s = buffer.getvalue()
     st.text(s)
     
-  st.header("Conteggio ") 
-  colonna = st.text_input("Su quale colonna occorre effettuare il conteggio ?")
+st.header("Conteggio ") 
+colonna = st.text_input("Su quale colonna occorre effettuare il conteggio ?")
   
-  st.write(":blue[df['colonna'].value_counts()]")
+st.write(":blue[df['colonna'].value_counts()]")
   
                                                   #conteggio = df["Failure Type"].value_counts()
-  if colonna:
+if colonna:
     conteggio = df[colonna].value_counts()
     st.text(conteggio)
   
-  st.header("Valori univoci ")
-  univoco = st.text_input("Su quale colonna occorre estrarre il valore univoco ?")
-  st.write(":blue[df['colonna'].sort_values().unique().tolist()]")#:blue[2 Data]
-  if univoco:
+st.header("Valori univoci ")
+univoco = st.text_input("Su quale colonna occorre estrarre il valore univoco ?")
+st.write(":blue[df['colonna'].sort_values().unique().tolist()]")#:blue[2 Data]
+if univoco:
                                                 #univoci = df["Failure Type"].unique()
                                                 #list_sub_category = df["Failure Type"].sort_values().unique().tolist()
     list_sub_category = df[univoco].sort_values().unique().tolist()
                                                 #st.text(list_sub_category)
     st.write(list_sub_category)
   
-  st.header("Raggruppamento ")
-  raggruppamento = st.text_input("Su quale colonna occorre effettuare in raggruppamento ?")
-  st.write(":blue[df.groupby('colonna')[\"Type\"].count()]")
-  if raggruppamento:
+st.header("Raggruppamento ")
+raggruppamento = st.text_input("Su quale colonna occorre effettuare in raggruppamento ?")
+st.write(":blue[df.groupby('colonna')[\"Type\"].count()]")
+if raggruppamento:
     gruppo = df.groupby(raggruppamento)["Type"].count()
                                                 #gruppo = df.groupby("Failure Type")["Type"].count()
                                                 #st.text(gruppo)
     st.write(gruppo)
 
-  st.header("Raggruppamento con media valori")
-  raggruppamedia = st.text_input("Su quale colonna occorre effettuare in raggruppamento con media valori ?")
-  if raggruppamedia:
+st.header("Raggruppamento con media valori")
+raggruppamedia = st.text_input("Su quale colonna occorre effettuare in raggruppamento con media valori ?")
+if raggruppamedia:
     
     st.write(":blue[df.groupby('colonna').agg({'Air temperature [K]':'median', 'Process temperature [K]':'median'})]")
   
