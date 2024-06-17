724f0f8af0516fdaf6260d8588008b7e1d38642a
161spok <136721685+161spok@users.noreply.github.com>
2024-06-17 19:36:32 +0200

    Update appo.py

--- pages/appo.py
+++ pages/appo.py
@@ -1,11 +1,72 @@
 import streamlit as st
 #from IPython.display import HTML
 import pandas as pd
-				
+#-----------------------------------------------------------------				
 with st.expander("See details."): 
     st.markdown('<p class="small-font">This is some text with a smaller font size.</p>', unsafe_allow_html=True)
+#-----------------------------------------------------------------
+uploaded_file = st.file_uploader("Scegli un file", key="pdf_uploader")
+  if st.button("Submit & Process", type="primary", key="process_button") :
+            with st.spinner("Elaborazione ..."):
+                                        st.success("Caricamento effettuato !") 
+                                        if uploaded_file is not None:                                           
+                                           
+                                            data = pd.read_csv(uploaded_file) #path folder of the data file
+                                            st.write(data)
+                                            tdata = pd.DataFrame(data)
+                                          
+                                          # media tutte le colonne escluso quelle non numeriche
+                                            media = tdata.mean(axis = 0, skipna = False, numeric_only=True)
+                                          
+                                          # media di colonne selezionate
+                                            #mean1 = tdata.iloc[:,0].mean() #calcola la media della prima colonna
+                                            mean2 = tdata.iloc[:,3].mean() # calcola la media della seconda colonna
+                                            st.write("Media della colonna 4")
+                                            st.write(mean2)
+                                            
+                                          # deviazione standard 
+              # In generale, una deviazione standard alta indica una maggiore variabilità dei dati, mentre una deviazione standard bassa indica una minore variabilità.
+                                            std_dev = tdata.std(numeric_only=True)
+                                            st.write("Deviazione standard su tutte le colonne")
+                                            st.write(std_dev)
+                                          
+                                          # deviazione standard su singola colonna
+                                            std_dev_col = tdata.iloc[:,3].std()
+                                            #  df['age'].std()
+                                            st.write("Deviazione standard su colonna 4")
+                                            st.write(std_dev_col)
+
+                                            # Generate values for the x-axis
+                                            # Calcola la densità di probabilità (PDF) della distribuzione normale
+                                            x = np.linspace(media - 3*std_dev, media + 3*std_dev, 1000)
+                                            pdf = norm.pdf(x, media, std_dev)
+                                            
+                                            # Visualizza la PDF
+                                            plt.plot(x, pdf)
+                                            plt.title('Distribuzione Normale dei Dati')
+                                            plt.xlabel('Valore')
+                                            plt.ylabel('Probabilità')
+                                            st.pyplot(plt.gcf())
+                                            
+                                            #------------------------------------------------------------------------                                        
+                                            x = np.linspace(0, 10000, 500)
+                                            arr = data['Type']
+                                            fig, ax = plt.subplots()
+                                            plt.title('Distribuzione Normale dei Dati colonna Type')
+                                            ax.hist(arr, bins=20)                                        
+                                            st.pyplot(fig)
+                                            #------------------------------------------------------------------------ 
+                                            figsize(7, 5)
+                                            plt.hist(data['Type'], color='blue', edgecolor='black', bins=int(45/1))
+                                            plt.xlabel('Quantità')
+                                            plt.ylabel('Tipologia')
+                                            plt.title('Housing prices frequencies')
+                                            st.pyplot(plt.gcf())
 
 
+
+
+#-----------------------------------------------------------------
 def load_column_data():
 	df = ""
 	uploaded_file = st.file_uploader("Scegli un file", key="pdf_uploader")
