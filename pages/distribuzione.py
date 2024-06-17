89a7a3b91fb756071a5e3009895befa4db3f6efc
161spok <136721685+161spok@users.noreply.github.com>
2024-06-17 18:54:03 +0200

    Update distribuzione.py

--- pages/distribuzione.py
+++ pages/distribuzione.py
@@ -83,24 +83,9 @@ else:
                                             st.pyplot(plt.gcf())
                                             
   #------------------------------------------------------------------------
-  #st.pyplot(df['Type'].value_counts().plot(kind='bar'))
+  st.pyplot(df['Type'].value_counts().plot(kind='bar'))
   
-  x = np.linspace(-10,10,100)
  
-  def normal_dist(x , mean , sd):
-      prob_density = np.exp(-0.5*((x-mean)/sd)**2) / np.sqrt(np.pi * sd**2)
-      return prob_density
-   
-  mean = st.slider(label='input mean', min_value=0., max_value=100., value=4.)
-  sd = st.slider(label='input std dev', min_value=0.1, max_value=100., value=4.)
-   
-  pdf = normal_dist(df['Type'], mean, sd)
-  
-  fig, ax = plt.subplots()
-  plt.plot(x, df['Type'] , color = 'blue')
-  plt.xlabel('Data points')
-  plt.ylabel('Probability Density')
-  st.pyplot(fig)
 
   
   col1, col2 = st.columns(2)
