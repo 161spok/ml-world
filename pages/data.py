8488fe99e9bd8cc6b8c7efa4c9fed94c3dd83144
161spok <136721685+161spok@users.noreply.github.com>
2024-06-17 15:26:38 +0200

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.header(":blue[2 Data]")
              
 st.subheader("Raccolta dati")
-dati = st.text_area('DESCRIZIONE', 'Questo è il processo di raccolta di dati da varie fonti, come sensori, database o altri sistemi. I dati possono essere strutturati o non strutturati e possono presentarsi in vari formati come testo, immagini o audio.')
+st.text_area('DESCRIZIONE', 'Questo è il processo di raccolta di dati da varie fonti, come sensori, database o altri sistemi. I dati possono essere strutturati o non strutturati e possono presentarsi in vari formati come testo, immagini o audio.')
 uploaded_file = st.file_uploader("Scegli un file", key="pdf_uploader")
 
 #---------------------------------------------------------------------------------
@@ -44,8 +44,8 @@ if st.button("Submit & Process", type="primary", key="process_button") :
                                             #data = pd.read_csv(uploaded_file) #path folder of the data file
                                             st.write(data)
                                             
-                                            #if 'dati' not in st.session_state:
-                                            #        st.session_state['dati'] = 'caricati'
+                                            if 'dati' not in st.session_state:
+                                                    st.session_state['dati'] = 'caricati'
                                             #        st.session_state.df = data
                                             #        st.write(st.session_state.df.shape[0])
 
