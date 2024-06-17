a407c9819a5dec226898ef57e3a8b4a13e7bb8ad
161spok <136721685+161spok@users.noreply.github.com>
2024-06-17 13:43:35 +0200

    Update preelaborazione.py

--- pages/preelaborazione.py
+++ pages/preelaborazione.py
@@ -78,7 +78,7 @@ st.markdown('<div class="my-container">This is a custom-styled container</div>',
 
 #https://ccaudek.github.io/bookdown_psicometria/chapter-descript.html#forma-di-una-distribuzione
 
-with st.expander('<div class="my-container">This is a custom-styled container</div>'):
+with st.expander("<div class='my-container'>This is a custom-styled container</div>"):
      st.write('''
         Immagina che tu abbia una grande ciotola di caramelle di diversi colori e vuoi sapere quale colore c’è di più. Allora inizi a separare 
         le caramelle per colore: rosse, blu, verdi, ecc. Dopo averle separate, conti quante caramelle ci sono di ogni colore. Magari scopri che hai 
